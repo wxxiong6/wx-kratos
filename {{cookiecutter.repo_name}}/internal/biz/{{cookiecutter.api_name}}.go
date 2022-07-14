f9f448081476d2dd5c39f66cc66e7ba664701443
wxxiong6 <wxxiong6@gmail.com>
2022-07-14 14:54:11 +0800

    update name

--- {{cookiecutter.repo_name}}/internal/biz/{{cookiecutter.api_name}}.go
+++ {{cookiecutter.repo_name}}/internal/biz/{{cookiecutter.api_name}}.go
@@ -3,7 +3,7 @@ package biz
 import (
 	"context"
 
-	v1 "first/api/{{cookiecutter.api_name}}/v1"
+	v1 "{{cookiecutter.module_name}}/api/helloworld/v1"
 	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
 )
@@ -13,33 +13,33 @@ var (
 	ErrUserNotFound = errors.NotFound(v1.ErrorReason_USER_NOT_FOUND.String(), "user not found")
 )
 
-// First is a First model.
-type First struct {
+// {{cookiecutter.service_name}} is a {{cookiecutter.service_name}} model.
+type {{cookiecutter.service_name}} struct {
 	Hello string
 }
 
-// FirstRepo is a Greater repo.
-type FirstRepo interface {
-	Save(context.Context, *First) (*First, error)
-	Update(context.Context, *First) (*First, error)
-	FindByID(context.Context, int64) (*First, error)
-	ListByHello(context.Context, string) ([]*First, error)
-	ListAll(context.Context) ([]*First, error)
+// {{cookiecutter.service_name}}Repo is a Greater repo.
+type {{cookiecutter.service_name}}Repo interface {
+	Save(context.Context, *{{cookiecutter.service_name}}) (*{{cookiecutter.service_name}}, error)
+	Update(context.Context, *{{cookiecutter.service_name}}) (*{{cookiecutter.service_name}}, error)
+	FindByID(context.Context, int64) (*{{cookiecutter.service_name}}, error)
+	ListByHello(context.Context, string) ([]*{{cookiecutter.service_name}}, error)
+	ListAll(context.Context) ([]*{{cookiecutter.service_name}}, error)
 }
 
-// FirstUsecase is a First usecase.
-type FirstUsecase struct {
-	repo FirstRepo
+// {{cookiecutter.service_name}}Usecase is a {{cookiecutter.service_name}} usecase.
+type {{cookiecutter.service_name}}Usecase struct {
+	repo {{cookiecutter.service_name}}Repo
 	log  *log.Helper
 }
 
-// NewFirstUsecase new a First usecase.
-func NewFirstUsecase(repo FirstRepo, logger log.Logger) *FirstUsecase {
-	return &FirstUsecase{repo: repo, log: log.NewHelper(logger)}
+// New{{cookiecutter.service_name}}Usecase new a {{cookiecutter.service_name}} usecase.
+func New{{cookiecutter.service_name}}Usecase(repo {{cookiecutter.service_name}}Repo, logger log.Logger) *{{cookiecutter.service_name}}Usecase {
+	return &{{cookiecutter.service_name}}Usecase{repo: repo, log: log.NewHelper(logger)}
 }
 
-// CreateFirst creates a First, and returns the new First.
-func (uc *FirstUsecase) CreateFirst(ctx context.Context, g *First) (*First, error) {
-	uc.log.WithContext(ctx).Infof("CreateFirst: %v", g.Hello)
+// Create{{cookiecutter.service_name}} creates a {{cookiecutter.service_name}}, and returns the new {{cookiecutter.service_name}}.
+func (uc *{{cookiecutter.service_name}}Usecase) Create{{cookiecutter.service_name}}(ctx context.Context, g *{{cookiecutter.service_name}}) (*{{cookiecutter.service_name}}, error) {
+	uc.log.WithContext(ctx).Infof("Create{{cookiecutter.service_name}}: %v", g.Hello)
 	return uc.repo.Save(ctx, g)
 }
