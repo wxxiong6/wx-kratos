65c919a38deb1cc9d1ed6ed13fb18d26884c148f
wxxiong6 <wxxiong6@gmail.com>
2023-08-23 14:36:33 +0800

    feat: change pkg path

--- {{cookiecutter.repo_name}}/internal/biz/biz.go
+++ {{cookiecutter.repo_name}}/internal/biz/biz.go
@@ -1,5 +1,13 @@
 package biz
 
-import "github.com/google/wire"
+import (
+	"context"
+	"github.com/google/wire"
+)
 
 var ProviderSet = wire.NewSet(New{{cookiecutter.service_name}}UseCase)
+
+type Transaction interface {
+	InTx(context.Context, func(ctx context.Context) error) error
+}
+
